1f90574b9ef9b82b91a298179da3bc4a4a49c0eb
Jason Golden <jason.golden@daydreamtechnologies.com>
2024-01-01 18:22:54 -1000

    Resolve Label Layout

--- Dymo_Connect_FoodStorage.js
+++ Dymo_Connect_FoodStorage.js
@@ -1,22 +1,24 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs').promises;
 
 async function printLabel(labelText) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    const axios = require('axios');
 
     // Load the Dymo Label Framework
     await page.addScriptTag({url: 'https://qajavascriptsdktests.azurewebsites.net/JavaScript/dymo.connect.framework.js'});
 
-    // Load your Dymo label file
-    let labelXml = await fs.readFile('/Users/jasongolden/Development/Label_Printer/Dymo_FoodStorage_Label.dymo', 'utf-8');
+// Load your Dymo label file
+const labelUrl = 'https://github.com/JasonGoldenDDT/SiriLabel/blob/dfc6ccc0defbe98a9ede4f855b92488318e09b9b/Dymo_FoodStorage_Label.dymo';
+const response = await axios.get(labelUrl);
+const labelXml = response.data;
 
-    // Set the label text and print the label
-    await page.evaluate((labelXml, labelText) => {
-        let label = dymo.label.framework.openLabelXml(labelXml);
-        label.setObjectText('FS_LabelTitle', labelText);
-        label.print('DYMO LabelWriter 450 Twin Turbo', '', dymo.label.framework.createLabelWriterPrintParamsXml({twinTurboRoll: 'Right'}));
-    }, labelXml, labelText);
+// Set the label text and print the label
+await page.evaluate((labelXml, labelText) => {
+    let label = dymo.label.framework.openLabelXml(labelXml);
+    label.setObjectText('TextObject0', labelText);
+    label.print('DYMO LabelWriter 450 Twin Turbo', '', dymo.label.framework.createLabelWriterPrintParamsXml({twinTurboRoll: 'Right'}));
+}, labelXml, labelText);
 
     await browser.close();
 }
